0270454d5e6013a1099d5adda7aade15b1500bad
Vladislav Kurkotov <kypa_2@ukr.net>
2017-12-24 17:26:42 +0200

    BT-2: add basic settings for user authentication

---
+++ BugTracker.Datalayer/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BugTracker.Datalayer.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BugTracker.Datalayer
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
